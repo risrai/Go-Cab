251af1468bab086bc5ff8e0b127b6a4b285c4331
mansii-agarwal <55138466+mansii-agarwal@users.noreply.github.com>
2020-02-04 15:27:41 +0530

    form validation

--- src/app/model/DriverModel.ts
+++ src/app/model/DriverModel.ts
@@ -1,7 +1,7 @@
 export class DriverModel{
     name: string;
-    age: number;
-    phone_number: number;
+    age: string;
+    phone_number: string;
     car_type: string;
     vehicle_no: string;
     exampleInputPassword1: string; 
