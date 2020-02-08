9c15b8643a695e51056b8475a58655a2698393a2
risrai <55137949+risrai@users.noreply.github.com>
2020-02-08 16:11:13 +0530

    angular part of sending booking data to the database is working

--- src/app/tranit/tranit.component.ts
+++ src/app/tranit/tranit.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BookingModel } from '../model/BookingModel.';
+import { BookingService } from '../booking.service';
 
 @Component({
   selector: 'app-tranit',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TranitComponent implements OnInit {
 
-  constructor() { }
+  booking= new BookingModel();
+  bookingObj:Object;
+  httpClient:HttpClient;
+  dataResponse: Object;
+
+  constructor(private bookingService:BookingService) { }
+
+  @ViewChild('bookingForm', { static: false }) form: any;
+  toastr: any;
 
   ngOnInit() {
   }
 
+  onDataReceived(data) {
+    this.dataResponse = data;
+
+  }
+
+  onSubmit()
+  {
+    this.bookingObj={"bookingId":this.booking.bookingId,"source":this.booking.source,
+    "destination":this.booking.destination,"cabType":this.booking.cabType}
+    this.bookingService.doBooking(this.bookingObj)
+    .subscribe(
+      data => {this.onDataReceived(data);
+      },
+       )
+  }
 }
