3adfc134c7b2a07d47c06dad408f835f6e26a70b
risrai <55137949+risrai@users.noreply.github.com>
2020-02-11 12:26:56 +0530

    Booking

--- src/app/tranit/tranit.component.ts
+++ src/app/tranit/tranit.component.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { BookingModel } from '../model/BookingModel.';
 import { BookingService } from '../booking.service';
 import { TranitService } from '../tranit.service';
+import { TransitModel } from '../model/TransitModel';
 
 @Component({
   selector: 'app-tranit',
@@ -11,7 +12,8 @@ import { TranitService } from '../tranit.service';
 })
 export class TranitComponent implements OnInit {
 
- booking = new BookingModel();
+  booking = new BookingModel();
+  bookingObj = new BookingModel();
   httpClient:HttpClient;
   dataResponse: Object;
 
@@ -19,8 +21,8 @@ export class TranitComponent implements OnInit {
 
   @ViewChild('bookingForm', { static: false }) form: any;
   toastr: any;
-  public carType;
-  public transits;
+  public carType : string;
+  public transits = new TransitModel();
   public hidden=true ;
 
   ngOnInit() {
@@ -34,8 +36,24 @@ export class TranitComponent implements OnInit {
   searchMyRide()
   {
     this.tranitService.searchMyRide(this.carType)
-    .subscribe(data=>{this.transits=data});
+    .subscribe(data=>{this.transits=data;
+      console.log(this.transits.cabType);});
     this.hidden=false ;
+    
+  }
+  
+
+  addDetails()
+  {
+
+    this.bookingObj={"source":this.booking.source,
+     "destination":this.booking.destination,"carType":this.transits.cabType};
+     console.log(this.bookingObj);
+     this.tranitService.doBooking(this.bookingObj)
+     .subscribe(
+       data => {this.onDataReceived(data);
+       },
+        )
   }
 
  
