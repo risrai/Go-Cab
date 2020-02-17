78825a32f40cdf5b279b671f33ea2f0717f166c2
risrai <55137949+risrai@users.noreply.github.com>
2020-02-17 13:39:00 +0530

    Sign up ka tab laga diya

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,9 +6,6 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { LogInComponent } from './log-in/log-in.component';
 import { TranitComponent } from './tranit/tranit.component';
 import { ListDriverComponent } from './list-driver/list-driver.component';
-import { ListRiderComponent } from './list-rider/list-rider.component';
-import { ListTransitComponent } from './list-transit/list-transit.component';
-import { PaymentComponent } from './payment/payment.component';
 import { AuthGuard } from './auth/auth.guard';
 import { 
   RoleGuardService as RoleGuard 
@@ -17,19 +14,20 @@ import { ListBookingComponent } from './list-booking/list-booking.component';
 
 
 const routes: Routes = [
-  {path: 'rir', component: RiderComponent},
-  {path:'driver',component: DriverComponent} ,
   {path:'about',component:HomePageComponent},
+  {path: 'rir', component: RiderComponent,canActivate:[AuthGuard]},
+  {path:'driver',component: DriverComponent,canActivate:[AuthGuard]} ,
   {path:'login',component:LogInComponent},
   {path:'transit',component:TranitComponent,canActivate:[AuthGuard]},
   {path:'list-driver',component:ListDriverComponent,canActivate:[RoleGuard],data:{roles:'ROLE_Admin'}},
-  {path:'list-rider',component:ListRiderComponent,canActivate:[AuthGuard]},
-  {path:'list-transit',component:ListTransitComponent,canActivate:[AuthGuard]},
-  {path: 'payment',component:PaymentComponent,canActivate:[AuthGuard]},
-  {path:'searchRide',component:ListTransitComponent,canActivate:[AuthGuard]},
-  {path:'list-payment',component:PaymentComponent},
-  {path:'goFirst',component:TranitComponent,canActivate:[AuthGuard]},
-  {path:'list-bookings',component:ListBookingComponent}
+  {path:'list-bookings',component:ListBookingComponent},
+  // {path:'list-rider',component:ListRiderComponent,canActivate:[AuthGuard]},
+  // {path:'list-transit',component:ListTransitComponent,canActivate:[AuthGuard]},
+  // {path: 'payment',component:PaymentComponent,canActivate:[AuthGuard]},
+  // {path:'searchRide',component:ListTransitComponent,canActivate:[AuthGuard]},
+  // {path:'list-payment',component:PaymentComponent},
+  {path:'goFirst',component:TranitComponent,canActivate:[AuthGuard]}
+ 
 ];
 
 @NgModule({
