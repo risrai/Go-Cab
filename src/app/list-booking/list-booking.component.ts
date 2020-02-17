81b1be5d70a060d0c3f95e91a1eb0ed1e7cdc8b4
risrai <55137949+risrai@users.noreply.github.com>
2020-02-17 13:15:17 +0530

    Sorting working in View Booking module

--- src/app/list-booking/list-booking.component.ts
+++ src/app/list-booking/list-booking.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { TranitService } from '../tranit.service';
 import { ListBookingService } from '../list-booking.service';
 import { ListBookingModel } from '../model/ListBookingModel';
+import { BookingModel } from '../model/BookingModel.';
 
 @Component({
   selector: 'app-list-booking',
@@ -9,6 +10,9 @@ import { ListBookingModel } from '../model/ListBookingModel';
   styleUrls: ['./list-booking.component.css']
 })
 export class ListBookingComponent implements OnInit {
+
+  sortButtonPressed:boolean;
+  sortSecondButtonPressed:boolean;
 
   constructor(private bookingService:ListBookingService) {
     this.bookingDetails=[] ;
@@ -23,6 +27,60 @@ export class ListBookingComponent implements OnInit {
     .subscribe(data => {
       this.bookingDetails=data;
     })
+  }
+
+  sortByFare(flag:boolean){
+    if(flag){
+    this.bookingDetails = this.bookingDetails.sort(
+      (e1,e2)=>{
+        if(e1.fare > e2.fare)
+          return 1;
+
+        if(e1.fare < e2.fare)
+          return -1;
+        return 0;
+      }
+    )
+  }
+  else{
+    this.bookingDetails = this.bookingDetails.sort(
+      (e1,e2)=>{
+        if(e1.fare < e2.fare)
+          return 1;
+
+        if(e1.fare > e2.fare)
+          return -1;
+        return 0;
+      }
+    )
+  }
+  }
+
+  sortByBookingId(flag:boolean){
+    if(flag){
+    this.bookingDetails = this.bookingDetails.sort(
+      (e1,e2)=>{
+        if(e1.bookingId > e2.bookingId)
+          return 1;
+
+        if(e1.bookingId < e2.bookingId)
+          return -1;
+        return 0;
+      }
+    )
+  }
+  else{
+    this.bookingDetails = this.bookingDetails.sort(
+      (e1,e2)=>{
+        if(e1.bookingId < e2.bookingId)
+          return 1;
+
+        if(e1.bookingId > e2.bookingId)
+          return -1;
+        return 0;
+      }
+    )
+  }
   }
 
 }
